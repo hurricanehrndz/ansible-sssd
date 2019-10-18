df8ccb0a4d1cabb3fb1f338b2a7a642aa22f05ae
Carlos Hernandez <carlos@techbyte.ca>
2019-10-18 09:18:30 -0600

    test sssd conf
    Signed-off-by: Carlos Hernandez <carlos@techbyte.ca>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,32 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_sssd_conf(host):
+    conf = host.file('/etc/sssd/sssd.conf')
+
+    assert conf.exists
+    assert conf.user == 'root'
+    assert conf.group == 'root'
+
+
+def test_sssd_conf_content(host):
+    conf = host.file('/etc/sssd/sssd.conf')
+    conf_content = conf.content
+
+    expected = [
+        b'config_file_version = 2',
+        b'services = nss, pam, ssh, sudo',
+        b'domains = contoso.com',
+        b'default_domain_suffix = contoso.com',
+        b'filter_groups = root',
+        b'filter_users = root',
+        b'reconnection_retries = 3',
+        b'[domain/contoso.com]',
+        b'ad_domain = contoso.com',
+        b'krb5_realm = contoso.com'
+    ]
+
+    for line in expected:
+        assert line in conf_content
