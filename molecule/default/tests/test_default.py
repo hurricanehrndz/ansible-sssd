a7f8f46889d2b5f9f85ad7636db3376a3c3d399f
Carlos Hernandez <carlos@techbyte.ca>
2019-10-22 16:59:52 -0600

    Configure and test samba and apparmor requirments for sssd-ad
    Signed-off-by: Carlos Hernandez <carlos@techbyte.ca>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -41,3 +41,44 @@ def test_sssd_conf_content(host):
 
     for line in expected:
         assert line in conf_content
+
+
+def test_apparmor_home_tunable(host):
+    conf = host.file('/etc/apparmor.d/tunables/home.d/ansible')
+
+    assert conf.exists
+    assert conf.user == 'root'
+    assert conf.group == 'root'
+    assert conf.mode == 0o644
+
+    conf_content = conf.content
+    expected_line = b'@{HOMEDIRS}+=/home/contoso.com/'
+    assert expected_line in conf_content
+
+
+def test_smb_conf(host):
+    conf = host.file('/etc/samba/smb.conf')
+
+    assert conf.exists
+    assert conf.user == 'root'
+    assert conf.group == 'root'
+    assert conf.mode == 0o644
+
+
+def test_smb_conf_contents(host):
+    conf = host.file('/etc/samba/smb.conf')
+    conf_content = conf.content
+
+    expected = [
+        b'realm = CONTOSO.COM',
+        b'workgroup = CONTOSO',
+        b'idmap config CONTOSO:backend = ad',
+        b'idmap config CONTOSO:schema_mode = rfc2307',
+        b'idmap config CONTOSO:range = 10000-999999',
+        b'idmap config CONTOSO:unix_nss_info = yes',
+        b'kerberos method = secrets and keytab',
+        b'security = ads'
+    ]
+
+    for line in expected:
+        assert line in conf_content
